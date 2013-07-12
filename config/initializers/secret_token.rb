06aba1190a7fa5234cf3441e5dbe3b4363ac94d7
Elijah Gartin <elijah.gartin@gmail.com>
2013-07-11 20:38:20 -0600

    Rails and Scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '333eb32c3343483687eab017b4b558b15301dbce994f6e5b1373ba14e7957c1d5961f3ca893a88eec4b5accc992704c1a9abad6d5dcf113144e3e84f3678065d'
